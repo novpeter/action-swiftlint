7280b8ce263e4843fed9f5018f9fcf37e8e3d6bc
Petr Novoselov <novpeter@yandex.ru>
2021-05-31 21:46:09 +0300

    Fix saving in working directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,4 +31,4 @@ then
 	fi
 fi
 
-set -o pipefail && swiftlint "$@" -- $changedFiles | stripPWD | convertToGitHubActionsLoggingCommands | tee ./Logs/SwiftLintResult.txt
+set -o pipefail && swiftlint "$@" -- $changedFiles | stripPWD | convertToGitHubActionsLoggingCommands | tee ${WORKING_DIRECTORY}/SwiftLintResult.txt
